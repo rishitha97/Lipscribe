ed05663938e84215efbaeabcd12045c103a21f83
rishitha97 <70471602+rishitha97@users.noreply.github.com>
2021-10-29 18:49:46 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,191 @@
+package com.example.myapplication;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.chaquo.python.PyObject;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    private final int VIDEO_RECORD_CODE=101;
+    private static int CAMERA_PERMISSION_CODE = 100;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    private Uri videoPath;
+    TextView textView;
+
+    private void recordVideo(){
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        activityResultLauncher.launch(intent);
+    }
+    public void captureVideo(View view)
+    {
+        recordVideo();
+    }
+
+    private boolean isCameraPresentInPhone(){
+        if(getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+            return true;
+        }
+            else
+            {
+                return false;
+            }
+        }
+    private void getCameraPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        if(isCameraPresentInPhone())
+        {
+            Log.i("VIDEO_RECORD_TAG", "Camera is Detected");
+            getCameraPermission();
+        }
+        else
+        {
+            Log.i("VIDEO_RECORD_TAG", "No Camera is not Detected");
+        }
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    videoPath = result.getData().getData();
+                    Log.i("Video_URI", "URI new is "+videoPath);
+//                    path is content://media/external/video/media/44
+                    Log.i("Video_Path", "Path new  is "+getPath(videoPath));
+                    String path= getPath(videoPath);
+                    Log.i("Video_Path", "Path new  is "+path);
+                    executePython(path);
+                    Log.i("Video_Path", "Path new  is "+path);
+//                    /storage/emulated/0/Movies/VID_20211028_151758.mp4
+                }
+            }
+        });
+    }
+
+    public void executePython(String video_path)
+    {
+        if(!Python.isStarted())
+        {
+            Python.start(new AndroidPlatform(this));
+        }
+        Python py = Python.getInstance();
+        Log.i("video_path","video path is "+video_path);
+//        PyObject pyObj = py.getModule("helloWorld");
+//        PyObject obj = pyObj.callAttr("main");
+        Log.i("video_path","video path is before getting module "+video_path);
+        PyObject pyObj = py.getModule("preprocess");
+        Log.i("video_path","video path is after getting module "+video_path);
+        PyObject obj = pyObj.callAttr("video_to_npy_array",video_path);
+        //Log.i("python test", "msg is"+obj.toString());
+
+    }
+
+    public String getPath(Uri uri) {
+        Cursor cursor = null;
+        try {
+            String[] projection = {MediaStore.Images.Media.DATA};
+            cursor = getContentResolver().query(uri, projection, null, null, null);
+
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
